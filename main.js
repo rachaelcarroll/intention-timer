04d8f7ce4418d0e75124f6558ab00241b95951cf
Fara Akhatova <faraakhatova@gmail.com>
2021-04-15 16:53:38 -0600

    Add variables for input fields and event listener to start activity button

--- main.js
+++ main.js
@@ -3,13 +3,16 @@
   var studyBtn = document.getElementById('studyButton')
   var meditateBtn = document.getElementById('meditateButton')
   var exerciseBtn = document.getElementById('exerciseButton')
-
-
-
-
+  var goalInput = document.getElementById('goals');
+  var minutesInput = document.getElementById('minutes');
+  var secondsInput = document.getElementById('seconds');
+  var categoryError = document.getElementById('categoryError')
+  var descriptionError = document.getElementById('descriptionError')
+  var timeError = document.getElementById('timeError')
+  var startActivityBtn = document.getElementById('startActivity')
   // EVENT LISTENERS //
   categoryContainer.addEventListener('click', changeColor)
-
+  startActivityBtn.addEventListener('click', startActivity)
 
   // EVENT HANDLERS AND GLOBAL FUNCTIONS//
   function changeColor(event) {
@@ -35,4 +38,22 @@
 
   function removeColor(button, activeClass) {
     button.classList.remove(activeClass)
+  }
+
+
+  function startActivity() {
+    event.preventDefault()
+    checkInputs()
+  }
+
+  function checkInputs() {
+    if (goalInput.value === "") {
+      descriptionError.classList.remove('hidden')
+      console.log(goalInput.value)
+    }
+
+
+
+    console.log(minutesInput.value);
+    console.log(secondsInput.value)
   }
