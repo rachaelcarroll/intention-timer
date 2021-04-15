3f575acfd1ccba7e90c597a8eeeb12e9847cf9de
Sydney Bear <sbear3915@gmail.com>
2021-04-15 15:16:54 -0600

    CSS/ HTML layout

---
+++ Activity.js
@@ -0,0 +1,10 @@
+class Activity {
+  constructor(category, description, minutes, seconds, id){
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.completed = null;
+    this.id = date.now;
+  }
+}
