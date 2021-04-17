35e7e1af4adbfb6d9bf6d9ffa97d423ed384e010
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-04-17 14:54:53 -0400

    Create countdown function

--- Activity.js
+++ Activity.js
@@ -8,15 +8,36 @@ class Activity {
     this.id = Date.now();
   }
 
-  countdown(){
 
-  }
+    //we need seconds to decrease by 1, and once it hits 0, minutes needs to decrease by 1
+    //when timer is 00:00
+    //window alert that
+    countdown(duration, display) {
+      startTimerBtn.disabled = true;
+      var timer = duration, minutes, seconds;
+      var time = setInterval(function () {
+          minutes = parseInt(timer / 60);
+          seconds = parseInt(timer % 60);
 
-  markComplete(){
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  }
+          display.textContent = minutes + ":" + seconds;
+
+          if (--timer < 0) {
+            timer = 0;
+            clearInterval(time);
+          }
+
+      }, 1000);
+    }
+
+  // markComplete(){
+  // this.completed = true
+  // startTimer.innerText = "COMPLETE!"
+  // }
 
   saveToStorage(){
-    
+
   }
 }
